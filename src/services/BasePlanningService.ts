5572997f6c9ef2e60affa0823d4bac975dbda47e
Djane Rey Mabelin <d.mabelin@arcanys.com>
2025-07-05 01:05:22 +0800

    Base building
    Construction site algorithms.

--- src/services/BasePlanningService.ts
+++ src/services/BasePlanningService.ts
@@ -7,10 +7,11 @@ declare global {
   interface RoomMemory {
     bp?: {
       distTrans?: number[],
-      costMat?: number[],
       allocated?: number[],
       upgrade?: StampBox,
       core?: StampBox,
+      costMat?: number[],
+      constructionSites?: number[][]
     }
   }
 }
@@ -33,16 +34,19 @@ export class BasePlanningService implements ServiceInterface {
           if(!room.memory.bp) room.memory.bp = {};
           if(!room.memory.bp.distTrans){
             this.findDistTrans(room);
+          }else if(room.memory.bp.distTrans && !room.memory.bp.upgrade){
+            this.findUpgrade(room);
+          }else if(room.memory.bp.distTrans && !room.memory.bp.core){
+            this.findCore(room);
           }else if(!room.memory.bp.costMat){
             this.findCostMat(room);
-          }else if(room.memory.bp.distTrans && room.memory.bp.costMat && !room.memory.bp.upgrade){
-            this.findUpgrade(room);
-          }else if(room.memory.bp.distTrans && room.memory.bp.costMat && !room.memory.bp.core){
-            this.findCore(room);
+          }else if(!room.memory.bp.constructionSites){
+            this.findBuildingSites(room);
           }
 
           if(this.debug && room.memory.bp.upgrade) this.renderStamp(room, room.memory.bp.upgrade)
           if(this.debug && room.memory.bp.core) this.renderStamp(room, room.memory.bp.core)
+          if(this.debug && room.memory.bp.constructionSites) this.renderConstructionSites(room, room.memory.bp.constructionSites)
 
           return {scheduleIn: {id: `bp.${roomId}`, t: 1}}
         }
@@ -54,17 +58,6 @@ export class BasePlanningService implements ServiceInterface {
     if(!room.memory.bp) room.memory.bp = {}
     const distTrans = getDistanceTransform(room.name, {visual: this.debug});
     room.memory.bp.distTrans = distTrans.serialize();
-  }
-
-  findCostMat(room: Room){
-    if(!room.memory.bp) room.memory.bp = {}
-    const positions = room.find(FIND_SOURCES).map(s => s.pos);
-
-    if(room.controller){
-      positions.push(room.controller.pos);
-    }
-    const costMat = getPositionsByPathCost(room.name, positions, {visual: this.debug});
-    room.memory.bp.costMat = costMat.serialize();
   }
 
   /**
@@ -127,6 +120,91 @@ export class BasePlanningService implements ServiceInterface {
     this.stampBoxAllocate(room, room.memory.bp.core)
   }
 
+  findCostMat(room: Room){
+    if(!room.memory.bp) room.memory.bp = {}
+    if(!room.memory.bp.core) return;
+    const coreCenterPos = room.getPositionAt(room.memory.bp.core.x, room.memory.bp.core.y);
+    if(!coreCenterPos) return;
+    const costMat = getPositionsByPathCost(room.name, [coreCenterPos], {visual: this.debug});
+    room.memory.bp.costMat = costMat.serialize();
+  }
+
+  findBuildingSites(room: Room){
+    if(!room.memory.bp) room.memory.bp = {}
+    if(!room.memory.bp.core) return;
+    if(!room.memory.bp.allocated) return
+    if(!room.memory.bp.costMat) return
+    const distTrans = PathFinder.CostMatrix.deserialize(room.memory.bp.distTrans || []);
+    const allocation = PathFinder.CostMatrix.deserialize(room.memory.bp.allocated)
+    const costMat = PathFinder.CostMatrix.deserialize(room.memory.bp.costMat)
+    let open : number[][] = [[room.memory.bp.core.x, room.memory.bp.core.y]]
+    let close : number[][] = []
+    let diagStep = 5;
+    let first = true;
+    const visited = new PathFinder.CostMatrix();
+    let itr = 0
+    while(true){
+      // fill open
+      let tmpOpen: number[][] = [];
+      if(open.length === 0) break
+
+      // work on open, new open in temporary
+      open.forEach(([x, y]) => {
+        visited.set(x, y, 1);
+        [
+          [x-diagStep, y-diagStep],
+          [x+diagStep, y-diagStep],
+          [x-diagStep, y+diagStep],
+          [x+diagStep, y+diagStep],
+        ].forEach(([x,y]) => {
+          if(x < 1 || x > 49) return;
+          if(y < 1 || y > 49) return;
+          if(visited.get(x,y) > 0) return;
+          if(costMat.get(x,y) > 10) return;
+          tmpOpen.push([x,y]);
+          visited.set(x,y, 1);
+        })
+
+      });
+
+
+      if(first && tmpOpen.length > 0){
+
+        tmpOpen = [tmpOpen[0]];
+        first = false;
+      }
+      // open to close
+      close = close.concat(tmpOpen)
+      // temporary open to open
+      open = tmpOpen;
+      diagStep = 2;
+    }
+    const constructionSites: number [][] = [];
+
+    close.sort((a, b) => {
+      return costMat.get(a[0], a[1]) - costMat.get(b[0], b[1])
+    })
+
+    close.forEach(([x,y]) => {
+      if(room.getTerrain().get(x, y) > 0) return;
+      if(distTrans.get(x,y) <= 1) return;
+      if(allocation.get(x,y) > 0) return;
+      [
+        [x,y],
+        [x,y-1],
+        [x,y+1],
+        [x-1,y],
+        [x+1,y],
+      ].forEach(([x,y]) => {
+        constructionSites.push([x,y])
+        allocation.set(x,y, 1)
+      })
+    })
+
+    room.memory.bp.constructionSites = constructionSites
+
+  }
+
   private renderStamp(room: Room, stamp: StampBox){
     room.visual.rect(stamp.x-stamp.r, stamp.y-stamp.r, stamp.r*2, stamp.r*2)
   }
@@ -158,5 +236,38 @@ export class BasePlanningService implements ServiceInterface {
       }
     }
     return false;
+  }
+  private isGridAllocated(room: Room, x:number, y: number, allocations: CostMatrix): boolean{
+    if (!room.memory.bp) return false;
+    if (!room.memory.bp.allocated) return false;
+
+
+    return !![
+      [x, y],
+      [x, y-1],
+      [x, y+1],
+      [x-1, y],
+      [x+1, y],
+    ].find(([x, y]) => {
+      return allocations.get(x, y) > 0;
+    })
+  }
+
+  private allocateGrid(room: Room, x:number, y: number, allocations: CostMatrix){
+    if (!room.memory.bp) return;
+    if (!room.memory.bp.allocated) return;
+    [
+      [x, y],
+      [x, y-1],
+      [x, y+1],
+      [x-1, y],
+      [x+1, y],
+    ].forEach(([x, y]) =>  allocations.set(x, y, 1))
+  }
+
+  private renderConstructionSites(room: Room, constructionSites: number[][]){
+    constructionSites.forEach(([x,y]) => {
+      room.visual.circle(x, y)
+    })
   }
 }
